aaaeb715df3efdabb1895360acc0d1ef7f779a71
Adkinsm <76773401+adkinsmpage@users.noreply.github.com>
2021-05-05 09:54:30 +0800

    Add files via upload

---
+++ source/links/index.md
@@ -0,0 +1,9 @@
+---
+title: 友人帐
+date: 2021-04-17 11:38:09
+layout: links
+---
+
+请注意！！！
+
+这里友链仅供展示主体功能，真实友链在 [Adkinsm 主页](https://www.adkinsm.top/)
---
+++ themes/nanoas/scripts/index.js
@@ -0,0 +1,32 @@
+const logger = require('hexo-log')()
+
+/**
+ * Load Config
+ */
+
+ hexo.on("generateBefore", () => {
+  require("./config")(hexo);
+});
+
+
+/**
+ * Print welcome message
+ */
+
+ hexo.on('ready', () => {
+    logger.info(`
+
+
+__   _       ___   _____   __   _   _____       ___   _____  
+|  \ | |     /   | /  _  \ |  \ | | /  _  \     /   | /  ___/ 
+|   \| |    / /| | | | | | |   \| | | | | |    / /| | | |___  
+| |\   |   / /-| | | | | | | |\   | | | | |   / /-| | \___  \ 
+| | \  |  / /--| | | |_| | | | \  | | |_| |  / /--| |  ___| | 
+|_|  \_| /_/   |_| \_____/ |_|  \_| \_____/ /_/   |_| /_____/ 
+
+                    Made By Adkinsm
+
+                   Open Source By MIT
+
+               Thank You For Use NANOAS !
+`)});
