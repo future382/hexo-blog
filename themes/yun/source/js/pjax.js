26d0f1ad8d4d55a667a5ce992d5d0e4f52d1ac27
Adkinsm <76773401+adkinsmpage@users.noreply.github.com>
2021-08-23 17:46:31 +0800

    Update pjax.js

--- themes/yun/source/js/pjax.js
+++ themes/yun/source/js/pjax.js
@@ -31,4 +31,4 @@ document.addEventListener("DOMContentLoaded", initPjax);
 document.addEventListener("DOMContentLoaded", isHome);
 document.addEventListener("pjax:success", onPjaxSuccess);
 document.addEventListener("pjax:send", () => {NProgress.start();});
-document.addEventListener("pjax:complete", () => {debugger;NProgress.done();});
+document.addEventListener("pjax:complete", () => {NProgress.done();});
