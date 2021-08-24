77d06835c296a52db14ca27685c3ba6843983b48
Adkinsm <76773401+adkinsmpage@users.noreply.github.com>
2021-08-24 10:40:03 +0800

    Update hexo-theme-yun.js

--- themes/yun/source/js/hexo-theme-yun.js
+++ themes/yun/source/js/hexo-theme-yun.js
@@ -41,33 +41,3 @@ document.addEventListener("DOMContentLoaded", initPage);
          }, 2000);
      }
  });
-
-document.body.addEventListener('copy', function (e) {
-    if (window.getSelection().toString() && window.getSelection().toString().length > 42) {
-        setClipboardText(e);
-        alert('商业转载请联系作者获得授权，非商业转载请注明出处哦~\n\n谢谢合作~(｡・`ω´･)');
-    }
-}); 
-function setClipboardText(event) {
-    var clipboardData = event.clipboardData || window.clipboardData;
-    if (clipboardData) {
-        event.preventDefault();
-        var htmlData = ''
-            + '著作权归作者所有。<br>'
-            + '商业转载请联系作者获得授权，非商业转载请注明出处。<br>'
-            + '作者：Adkinsm <br>'
-            + '链接：' + window.location.href + '<br>'
-            + '来源：Adkinsm 部落格<br><br>'
-            + window.getSelection().toString();
-        var textData = ''
-            + '著作权归作者所有。\n'
-            + '商业转载请联系作者获得授权，非商业转载请注明出处。\n'
-            + '作者：Adkinsm\n'
-            + '链接：' + window.location.href + '\n'
-            + '来源：Adkinsm 部落格\n\n'
-            + window.getSelection().toString();
- 
-        clipboardData.setData('text/html', htmlData);
-        clipboardData.setData('text/plain',textData);
-    }
-}
