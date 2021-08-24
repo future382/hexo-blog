7c5315b837fd0ebe8516096ca4718cd11176f4d1
Adkinsm <76773401+adkinsmpage@users.noreply.github.com>
2021-08-24 10:33:12 +0800

    Update hexo-theme-yun.js

--- themes/yun/source/js/hexo-theme-yun.js
+++ themes/yun/source/js/hexo-theme-yun.js
@@ -23,3 +23,21 @@ function initPage() {
 
 Yun.boot.copyright();
 document.addEventListener("DOMContentLoaded", initPage);
+
+// 下面是Adkinsm新增内容
+
+ let OriginTitle = document.title,
+     titleTime;
+
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         document.title = '502 | 页面已崩溃！';
+         clearTimeout(titleTime);
+     }
+     else {
+         document.title = '已恢复连接！' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
