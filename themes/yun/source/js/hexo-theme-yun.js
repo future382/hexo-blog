f23fbc555495444a4f081284685e51d41d619928
Adkinsm <76773401+adkinsmpage@users.noreply.github.com>
2021-09-12 14:08:25 +0800

    Update hexo-theme-yun.js

--- themes/yun/source/js/hexo-theme-yun.js
+++ themes/yun/source/js/hexo-theme-yun.js
@@ -44,7 +44,8 @@ document.addEventListener("DOMContentLoaded", initPage);
 
 !function(){var e=/([http|https]:\/\/[a-zA-Z0-9\_\.]+\.baidu\.com)/gi,r=window.location.href,t=document.referrer;if(!e.test(r)){var o="https://sp0.baidu.com/9_Q4simg2RQJ8t7jm9iCKT-xh_/s.gif";t?(o+="?r="+encodeURIComponent(document.referrer),r&&(o+="&l="+r)):r&&(o+="?l="+r);var i=new Image;i.src=o}}(window);
 
-
+document.onload = function(){
+  
 // baguetteBox Libs
 var baguetteBox = function() {
     function t(t, n) {
@@ -229,3 +230,4 @@ baguetteBox.run('.post-content。markdown-body', {
             }
         });
 
+}
